94dcb4219c5c25c4b202a3529c9a9590ad4de94e
Lucho3004 <lgradosg@hotmail.com>
2016-06-22 22:07:17 -0500

    Excluidos

---
+++ WebDeveloper/Controllers/RazorController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebDeveloper.DataAccess;
+using WebDeveloper.Model;
+
+namespace WebDeveloper.Controllers
+{
+    public class RazorController : Controller
+    {
+
+        // GET: Razor
+        public ActionResult Index()
+        {
+            var clients = new List<Client>
+            {
+                new Client { ID = 1, LastName = "Cueva", Name = "Aladino" },
+                new Client { ID = 2, LastName = "Ruidiaz", Name = "Raul" },
+                new Client { ID =3, LastName = "Peredo", Name = "José" }
+            };
+
+
+            return View(clients);
+        }
+        public ActionResult Product()
+        {
+            var productData = new ProductData();
+            return View(productData.GetFakeProducts());
+        }
+
+    }
+}
