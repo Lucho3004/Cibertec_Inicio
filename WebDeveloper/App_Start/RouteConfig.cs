bddbcddbe23c767b85265dc1edff77696106d1a6
Lucho3004 <lgradosg@hotmail.com>
2016-06-22 22:30:03 -0500

    Enturamiento

--- WebDeveloper/App_Start/RouteConfig.cs
+++ WebDeveloper/App_Start/RouteConfig.cs
@@ -12,12 +12,28 @@ namespace WebDeveloper
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.MapRoute(
+                   name: "Razor",
+                   url: "Razor/{id}",
+                   defaults: new
+                   {
+                       controller = "Razor",
+                       action = "Product",
+                       id = UrlParameter.Optional
+                   }
+               );
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new
+                {
+                    controller = "Home",
+                    action = "Index",
+                    id = UrlParameter.Optional
+                }
             );
+
+   
         }
     }
 }
