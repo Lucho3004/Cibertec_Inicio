dd451a68b0c5b9ba1beaa1ad23ae53e2b4c93d9c
Lucho3004 <lgradosg@hotmail.com>
2016-07-11 22:07:10 -0500

    Roles

---
+++ WebDeveloper/Controllers/BaseAccountController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebDeveloper.Controllers
+{
+    public class BaseAccountController : Controller
+    {
+        // GET: BaseAccount
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
